f6b94783a9552e9b645a619f6f73bd22786c6e58
kaiofelipedev <kaio.felipe.santos@hotmail.com>
2025-08-04 17:11:44 -0300

    Layout tela de produtos(ok).04/08/25-17:11

--- Catalogo_Mobile/src/components/card.tsx
+++ Catalogo_Mobile/src/components/card.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
 export const Card = () => {
     return (
         <TouchableOpacity style={styles.card}>
-
+            <Image style={styles.imageCard}/>
+            <Text style={styles.tituloCard}>TÍTULO</Text>
+            <Text style={styles.descricao}>DESCRIÇÃO DO PRODUTO</Text>
+            <Text style={styles.priceCard}>PRICE</Text>
         </TouchableOpacity>
     )
 }
@@ -14,7 +17,34 @@ const styles = StyleSheet.create({
         width: '42%',
         height: 220,
         borderWidth: 1,
+        borderColor: 'gray',
         margin: 10,
         borderRadius: 10
+    },
+    imageCard: {
+        width: '100%',
+        height: '40%',
+        borderBottomWidth: 1,
+        borderColor: 'gray'
+    },
+    tituloCard: {
+        width: '100%',
+        height: 30,
+        padding: 5,
+        fontWeight: 'bold'
+    },
+    descricao: {
+        width: '100%',
+        height: 70,
+        padding: 8,
+        fontSize: 12,
+        color: 'gray'
+    },
+    priceCard: {
+        width: '100%',
+        height: '14%',
+        paddingTop: 7,
+        paddingLeft: 7,
+        fontWeight: 'bold'
     }
 })
